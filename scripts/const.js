2bf240c36de3af1d449d19112d5a11362d500db5
Luba-web <ldvasileva87@yandex.ru>
2022-10-10 20:37:03 +0300

    feature: adaptive header and footer

--- scripts/const.js
+++
@@ -1,5 +0,0 @@
-//переменные для header и бар меню
-const closeBtn = document.querySelector("#icon-close");
-const menuBtn = document.querySelector("#icon-menu");
-const logo = document.querySelector("#logo");
-const bar = document.querySelector(".header__container");
